33f6851a4d546e69937924f01582097536357a2f
mimrock <lados.denes@gmail.com>
2015-01-16 14:35:38 +0100

    Update chan.go

--- go_meetup/code/chan.go
+++ go_meetup/code/chan.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type Truck struct {
-	Cargo float64
-	//Input chan float64
-}
-
-func (t *Truck) Report() {
-	fmt.Println("My cargo weights", t.Cargo, "kg")
-}
-
-func (t *Truck) AddCargo(w float64) {
-	t.Cargo += w 
+func strGenerator(input string, output chan string, wait time.Duration) {
+	for {
+		output <- input
+		time.Sleep(wait)
+	}
 }
 
 func main() {
-	goliath := Truck{75.5}
-	goliath.Report()
+	chA := make(chan string)
+	chB := make(chan string)
+	done := make(chan bool)
+	quit := false
+	var str string
+	go strGenerator("a", chA, 1000 * time.Millisecond)
+	go strGenerator("A", chA, 800 * time.Millisecond)
+	go strGenerator("B", chB, 600 * time.Millisecond)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- true
+	}()
+	for quit == false{
+		select {
+		case str = <-chA:
+			fmt.Println("Message from chan A:", str)
+		case str = <-chB:
+			fmt.Println("Message from chan B:", str)
+		case <-done:
+			fmt.Println("Done.")
+			quit = true
+		}
 
+	}
 }
